b894062f62e0ddf3a0989d9e88c546ed0b2acf22
Danil <danila_monster@mail,ru>
2022-08-23 22:35:28 +0300

    задача_42

---
+++ Task003/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, 
+// которая будет преобразовывать десятичное число в двоичное.
+
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+
+int GetLentMirray(int number)     
+{
+    int len = 0;
+    while (number >= 1)                  
+    {
+        number = number / 2;
+        len++;  
+    }
+    return len;
+}
+
+int[] Translation(int number)
+{
+    int[] array = new int[GetLentMirray(number)];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[array.Length - 1 - i] = number % 2;
+        number = number / 2;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++ ) 
+        Console.Write($"{array[i]} ");    
+    Console.WriteLine();       
+}
+
+int [] myArray = Translation(45);
+PrintArray(myArray);
+//Console.WriteLine(GetLentMirray(8));
+
