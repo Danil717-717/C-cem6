8478d9ccd7efab24c315835a3a10cf0ac15dd19d
Danil <danila_monster@mail,ru>
2022-08-23 23:02:19 +0300

    задача_45

---
+++ Task005/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая будет создавать копию 
+/// заданного массива с помощью поэлементного копирования.
+
+
+int [] CreateArray(int length)     
+{
+    int[] array = new int[length];
+    for(int i = 0; i < length; i++)                   
+    {
+        array[i] = new Random().Next(0,1000);  
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++ ) 
+        Console.Write($"{array[i]} ");
+
+    Console.WriteLine();    
+           
+}
+
+int [] CoppyArray(int[] arr)
+{
+    int [] newArray = new int [arr.Length];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        newArray[i] = arr[i];
+    }
+    return newArray;
+}
+
+int [] array = CreateArray(7);
+PrintArray(array);
+
+PrintArray(CoppyArray(array));
